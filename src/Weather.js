cf3eceffb35d0c99bd8a956c50844e4b1002d721
Sadiyya-k <125921130+Sadiyya-k@users.noreply.github.com>
2023-07-07 14:43:17 +0100

    Updated HTML structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,58 +1,44 @@
-import React, { useState } from "react";
-import axios from "axios";
-
-
+import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  const [country, setCountry] = useState(" ");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
-
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSearch(event) {
-    event.preventDefault();
-    let apiKey = `6a48a550fc04f170639e60d52b8a6bc5`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${apiKey}`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function updateSearch(event) {
-    setCountry(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSearch}>
-      <input type="search" placeholder="Search" onChange={updateSearch} />
-      <input type="submit" value="Search" />
-    </form>
-  );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {Math.round(weather.humidity)}%</li>
-          <li>Wind: {Math.round(weather.wind)}km/h</li>
-          <li>
-            <img src={weather.icon} alt="Weather.description" />
-          </li>
-        </ul>
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>England</h1>
+      <ul>
+        <li>Friday 10:00</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://openweathermap.org/img/wn/01d@2x.png"
+            alt="Clear Sky"
+          />
+          15°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Percipitation: 10%</li>
+            <li>Humidity: 35%</li>
+            <li>Wind: 6 km/h</li>
+          </ul>
+        </div>
       </div>
-    );
-  } else {
-    return form;
-  }
+    </div>
+  );
 }
